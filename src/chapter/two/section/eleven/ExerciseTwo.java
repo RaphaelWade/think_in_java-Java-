e5fad4f00f52cd34a8e2ea6dd8c47c81c91f4829
Raphael Cha <130455696+RaphaelWade@users.noreply.github.com>
2024-03-22 21:14:07 +0800

    Update ExerciseTwo.java

--- src/chapter/two/section/eleven/ExerciseTwo.java
+++ src/chapter/two/section/eleven/ExerciseTwo.java
@@ -1,7 +1,7 @@
 /*
     问题：参照本章的HelloDate.java这个例子，创建一个"Hello, world"程序，该程序只要
 输出这句话即可。你所编写的类里只需一个方法（即“main”方法，在程序启动时被执行）。记住要
-把它设计炜static形式，并指定参数列表——即使根本不会用到这个列表。
+把它设计为static形式，并指定参数列表——即使根本不会用到这个列表。
  */
 package chapter.two.section.eleven;
 
