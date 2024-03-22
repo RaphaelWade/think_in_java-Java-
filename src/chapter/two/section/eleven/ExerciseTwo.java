5aee1b0055777771350f44862af193327a9eb1f8
raphael_wade <1578917094@qq.com>
2024-03-22 20:56:21 +0800

    完成第二张第十一小节的练习2

---
+++ src/chapter/two/section/eleven/ExerciseTwo.java
@@ -0,0 +1,12 @@
+/*
+    问题：参照本章的HelloDate.java这个例子，创建一个"Hello, World"程序，该程序只要
+输出这句话即可。你所编写的类里只需一个方法（即“main”方法，在程序启动时被执行）。记住要
+把它设为static形式，并指定参数列表——即使根本不会用到这个列表。
+*/
+package chapter.two.section.eleven;
+
+public class ExerciseTwo {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
