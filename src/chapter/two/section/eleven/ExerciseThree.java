bc5dadca4d494deb6f535992f90657749d4879ac
raphael_wade <1578917094@qq.com>
2024-03-23 21:30:36 +0800

    完成第二章第十一节的练习3

---
+++ src/chapter/two/section/eleven/ExerciseThree.java
@@ -0,0 +1,16 @@
+/*
+    问题：找出ATypeName的代码段（P25），将其改写成完整的程序，然后编译、运行。
+ */
+package chapter.two.section.eleven;
+
+public class ExerciseThree {
+    public static void main(String[] args) {
+        ATypeName atypeName = new ATypeName();
+    }
+}
+
+class ATypeName {
+    ATypeName() {
+        System.out.println("欢迎使用ATypeName类！");
+    }
+}
