ff078ded8dccb8387e5b26dd774b2f0be721f2cc
raphael_wade <1578917094@qq.com>
2024-03-26 21:41:22 +0800

    完成第三章第一节练习1

---
+++ src/chapter/three/section/one/ExerciseOne.java
@@ -0,0 +1,13 @@
+/*
+    问题：使用“简短的”和正常的打印语句来编写一个程序。
+ */
+package chapter.three.section.one;
+
+import static java.lang.System.out;
+
+public class ExerciseOne {
+    public static void main(String[] args) {
+        out.println("这是“简短的”打印语句。");
+        System.out.println("这是正常的打印语句。");
+    }
+}
