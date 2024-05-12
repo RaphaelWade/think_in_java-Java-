c976f038ffdd79b8fad36ba80dc2555239093485
raphael_wade <1578917094@qq.com>
2024-05-12 22:06:30 +0800

    完成第六章第一节练习1

---
+++ src/chapter/six/section/ExerciseOneInstance.java
@@ -0,0 +1,14 @@
+package chapter.six.section;
+
+import chapter.six.section.one.ExerciseOne;
+
+public class ExerciseOneInstance {
+    public static void main(String[] args) {
+        ExerciseOne test = new ExerciseOne();
+    }
+}
+/*
+输出：
+    This is class ExerciseOne!
+
+ */
