2ab06eeae1d0b3b1a7a66e811a7433e5f59cc010
raphael_wade <1578917094@qq.com>
2024-05-16 22:01:56 +0800

    完成第六章第二节练习4

---
+++ src/chapter/six/section/one/exercisefour/ExerciseFour.java
@@ -0,0 +1,16 @@
+/*
+    问题：展示protected方法具有包访问权限，但不是public。
+ */
+package chapter.six.section.one.exercisefour;
+
+public class ExerciseFour {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.run();
+    }
+}
+/*
+输出：
+    protected方法具有包访问权限！
+
+ */
---
+++ src/chapter/six/section/one/exercisefour/Test.java
@@ -0,0 +1,7 @@
+package chapter.six.section.one.exercisefour;
+
+public class Test {
+    protected void run() {
+        System.out.println("protected方法具有包访问权限！");
+    }
+}
