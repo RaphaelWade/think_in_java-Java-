7ae96145bdc7b9e12204b61d99c8662bc03f9eaf
raphael_wade <1578917094@qq.com>
2024-05-28 21:46:54 +0800

    完成第八章第二节练习2

---
+++ src/chapter/eight/section/two/exercisetwo/Square.java
@@ -0,0 +1,13 @@
+package chapter.eight.section.two.exercisetwo;
+
+public class Square extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Square.draw()");
+    }
+
+    @Override
+    public void erase() {
+        System.out.println("Square.erase()");
+    }
+}
